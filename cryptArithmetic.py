3229a7fabdc13895a32d6dc25efcc9b2894319e6
darubagus <darubagus@gmail.com>
2021-01-25 22:11:15 +0700

    udah work
    tapi gatau kenapa ini cepet banget

--- cryptArithmetic.py
+++ cryptArithmetic.py
@@ -1,63 +1,19 @@
 
 import time
 
-
 # listOfNode = [{letter, corresponding value}]
 use = [0 for i in range(10)]
 
 listOfWord = []
 
 # MENGECEK APAKAH
-# def isValid(listOfNode, count, word1, word2, word3):
-#     val1 = 0
-#     val2 = 0
-#     val3 = 0
-#     # j,i
-
-#     m = 1
-#     i = word1.len()-1
-#     while (i>=0):
-#         ch = word1[i]
-#         for j in range(count) :
-#             if (listOfNode[j][0] == ch):
-#                 break
-#         val2 += m * listOfNode[j][1]
-#         m *= 10
-#         i-=1
-
-#     m = 1
-#     i = word2.len()-1
-#     while (i>=0):
-#         ch = word2[i]
-#         for j in range(count) :
-#             if (listOfNode[j][0] == ch):
-#                 break
-#         val2 += m * listOfNode[j][1]
-#         m *= 10
-#         i-=1
-
-#     m = 1
-#     i = word3.len()-1
-#     while (i>=0):
-#         ch = word3[i]
-#         for j in range(count) :
-#             if (listOfNode[j][0] == ch):
-#                 break
-#         val2 += m * listOfNode[j][1]
-#         m *= 10
-#         i-=1
-
-#     if (val3 == (val1+val2)):
-#         return True
-#     else :
-#         return False
 
 def isValid(listOfNode, count, listOfWord) :
-    val = [0 for i in range(len(listOfWord)-1)]
+    val = [0 for i in range(len(listOfWord))]
     temp = 0
     totalVal=0
 
-    for z in range(len(listOfWord)-1):
+    for z in range(len(listOfWord)):
         m = 1
         i = len(listOfWord[z])-1
         while (i >= 0) :
@@ -71,52 +27,78 @@ def isValid(listOfNode, count, listOfWord) :
             i-=1
     
     # buat ngecek hasil
-    for i in range(len(listOfWord)-2) :
+    for i in range(len(listOfWord)-1) :
         totalVal += val[i]
     
+
     if (totalVal == val[len(listOfWord)-1]) :
         return True
     else:
         return False
 
 
-def Permutate(count, listOfNode, n, listOfWord) :
-    if (n == count-1): #Basis
+def Permutate(countChar, listOfNode, n, listOfWord, opCounter) :
+    if (n == countChar-1): #Basis
         for i in range(10):
             if (use[i] == 0):
                 listOfNode[n][1] = i
-                if (isValid(listOfNode,count,listOfWord)) :
+                if (isValid(listOfNode,countChar,listOfWord)) :
+                    print(listOfNode)
                     print("Solusi ditemukan\n")
-                    # for j in range(count) :
-                    #     print(listOfNode[j][0]+" = "+listOfNode[j][1]+"\n")
                     # print letter and its corresponding value
                     return True
-        return False
+        # return False
     else:
         for i in range(10):
             if (use[i]==0):
                 listOfNode[n][1] = i
                 use[i] = 1
-                if (Permutate(count,listOfNode, n+1,listOfWord)):
+                if (Permutate(countChar,listOfNode, n+1,listOfWord)):
                     return True
                 use[i] = 0
-        return False # only for precaution
+        # return False # only for precaution
 
 def problemSolver(listofWord):
     char = 0
+    length = []
+
+    for i in range(len(listOfWord)-1):
+        length.append(len(listOfWord[i]))
     
+    letterFrequency = [0 for i in range(26)]
+
+    for i in range(len(listOfWord)):
+        for j in range(len(listOfWord[i])):
+            letterFrequency[ord(listOfWord[i][j]) - ord('A')] += 1
+
+    # SEND
+    # 
+
+    for i in range(26):
+        if (letterFrequency[i] > 0) :
+            char +=1
+    
+    if (char > 10) :
+        # Invalid
+        print("Invalid input")
+    
+    # listOfNode = [{letter, corresponding value}]
+    listOfNode = []
+
+    for i in range(26):
+        j = 0
+        if (letterFrequency[i] > 0) :
+            # listOfNode[j][0] = chr(i + ord('A'))
+            listOfNode.append([chr(i + ord('A')), 0])
+            j += 1
+            # listOfNode.append([chr(i + ord('A')), 0])
+        
+    opCounter = 0
+
+    return Permutate(char, listOfNode, 0, listOfWord,opCounter)
+
 
 #MAIN PROGRAM
-
-
-# for testing purpose
-
-
-
-# START TIME
-startTime = time.time()
-
-# KAMUS
 
 # open file
 inString = open("input.txt", "r")
@@ -132,11 +114,12 @@ while (i < len(line)):
         
         listOfWord.append(cleanString)
     i+=1
-
 inString.close()
-    
-outString = open("output.txt", "w")
 
+# START TIME
+startTime = time.time()
 
+print(problemSolver(listOfWord))
 # HITUNG RUNNING TIME
 runningTime = (time.time() - startTime)
+print(runningTime)
